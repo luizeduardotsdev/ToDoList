6f688633642ffef9e2d0a326d085760cd9128410
luiz <luizeduardotsdev@gmail.com>
2025-09-22 16:08:42 -0300

    atualiza o handler

--- todolist/src/main/java/com/spring_boot/todolist/erros/ExceptionHandlerController.java
+++ todolist/src/main/java/com/spring_boot/todolist/erros/ExceptionHandlerController.java
@@ -11,7 +11,7 @@ public class ExceptionHandlerController {
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<String> handleHttpMessage(HttpMessageNotReadableException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMostSpecificCause().getMessage());
     }
 
 }
